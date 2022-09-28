6b3d9958fecdf9875327b159f06db3daf5daccf5
Yurino Oga <yurino.o@latona.io>
2022-09-29 00:49:44 +0900

    release

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # data-platfrom-masters-and-transactions-mysql-kube
-data-platfrom-masters-and-transactions-mysql-kube は、データ連携基盤において、マスタデータ、トランザクションデータを保存するテーブル群を維持管理するMySQLデータベースを、Kubernetes上で立ち上げ稼働させるための マイクロサービス です。    
-本リポジトリには、必要なマニフェストファイル等が入っています。  
+data-platfrom-masters-and-transactions-mysql-kube は、Kubernetes 上で MariaDB(MySQL) の Pod を立ち上げ稼働させるための マイクロサービス です。    
+本リポジトリには、必要なマニフェストファイルが入っています。  
+また、本リポジトリには、MySQLの初期設定と、Pod立ち上げ後のテーブルの作成に関する手順が含まれています。  
+AIONでは、MySQLは主に、エッジアプリケーションで発生した静的なデータを保持・維持するために用いられます。  
 
 ## 動作環境
 
 * OS: Linux OS  
+
 * CPU: ARM/AMD/Intel  
+
 * Kubernetes  
 
 
 ## data-platfrom-masters-and-transactions-mysql-kube を用いたアーキテクチャ  
 data-platfrom-masters-and-transactions-mysql-kube は、下記の黄色い枠の部分のリソースです。  
 ![mysql_dataplatform](docs/dataplatform-masters-and-transactions_architecture.drawio.png)  
+
+
--- setup-mysql.sh
+++ setup-mysql.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
-DB_USER_NAME="root" # DBのユーザー名を指定
-DB_USER_PASSWORD="root" # DBのパスワードを指定
+DB_ROOT_NAME="XXXXXXXX" # DBのルートユーザーを指定
+DB_ROOT_PATH="XXXXXXXX" # DBのルートパスを指定
+DB_USER_NAME="XXXXXXXX" # DBのユーザー名を指定
+DB_USER_PASSWORD="XXXXXXXX" # DBのパスワードを指定
 POD_NAME="data-platform-masters-and-transactions-mysql-kube" # Podの名前を指定
-DATABASE_NAME="DataPlatformPlantMySQLKube" # データベース名を指定
-# SQL_NAME="data_platform_plant_sql_general_data.sql" # sqlファイルの名前を指定
-SQL_FILES=( # sqlファイルの名前を複数指定
-  "data_platform_plant_sql_general_data.sql"
-  "data_platform_plant_sql_address_data.sql"
-  )
+DATABASE_NAME="DataPlatformMastersAndTransactionsMysqlKube" # データベース名を指定
+# SQL_FILES=( # sqlファイルの名前を複数指定
+#   "data_platform_plant_sql_general_data.sql"
+#   "data_platform_plant_sql_address_data.sql"
+#   )
 MYSQL_POD=$(kubectl get pod | grep ${POD_NAME} | awk '{print $1}')
 
-kubectl exec -it ${MYSQL_POD} -- bash -c "mysql -u${DB_USER_NAME} -p${DB_USER_PASSWORD} -e \"CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME} default character set utf8 ;\""
+kubectl exec -it ${MYSQL_POD} -- bash -c "mkdir -p /src"
 
-# kubectl exec -it ${MYSQL_POD} -- bash -c "mysql -u${DB_USER_NAME} -p${DB_USER_PASSWORD} -D ${DATABASE_NAME} < /src/${SQL_NAME}"
-
+# sqlファイルのコピー
 for ((i=0; i<${#SQL_FILES[@]}; i++)) do
-  kubectl exec -it ${MYSQL_POD} -- bash -c "mysql -u${DB_USER_NAME} -p${DB_USER_PASSWORD} -D ${DATABASE_NAME} < /src/${SQL_FILES[i]}"
+  kubectl cp ${SQL_FILES[i]} ${MYSQL_POD}:/src
 done
+
+# databaseの作成
+kubectl exec -it ${MYSQL_POD} -- bash -c "mysql -u${DB_ROOT_NAME} -p${DB_ROOT_PATH} -e \"CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME} default character set utf8 ;\""
+
+# userに権限の付与
+kubectl exec -it ${MYSQL_POD} -- bash -c "mysql -u${DB_ROOT_NAME} -p${DB_ROOT_NAME} -e \"GRANT ALL ON *.* TO ${DB_USER_NAME}@;\""
+
+# テーブルの作成
+#  for ((i=0; i<${#SQL_FILES[@]}; i++)) do
+#   kubectl exec -it ${MYSQL_POD} -- bash -c "mysql -u${DB_USER_NAME} -p${DB_USER_PASSWORD} -D ${DATABASE_NAME} < /src/${SQL_FILES[i]}"
+#  done
